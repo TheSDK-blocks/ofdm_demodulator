f42cc851f8b263af0d6d1afa8eb5b47d35a9e0d8
Marko Kosunen <marko.kosunen@aalto.fi>
2019-06-19 21:51:54 -0700

    Runs with ThESDK v1.2

--- ofdm_demodulator/__init__.py
+++ ofdm_demodulator/__init__.py
@@ -21,13 +21,27 @@ class ofdm_demodulator(verilog,thesdk):
     def __init__(self,*arg): 
         self.proplist = [ 'Rs' ];    # Properties that can be propagated from parent
         self.Rs =  100e6;            # Sampling frequency
-        self.A = IO();               # Pointer for input data
-        self.symbol_sync_in = IO();  # Pointer for input data
-        self._symbol_sync_out = IO(); # Pointer for input data
+        self.IOS=Bundle()
+        self.IOS.Members['A'] = IO();               # Pointer for input data
+        _=verilog_iofile(self,name='A', dir='in', ionames=['io_A_real', 'io_A_imag' ])
+
+        self.IOS.Members['symbol_sync_in'] = IO();  # Pointer for input data
+        _=verilog_iofile(self,name='symbol_sync_in', dir='in', ionames=['io_symbol_sync_in'])
+
+        self.IOS.Members['symbol_sync_out'] = IO(); # Pointer for input data
+        _=verilog_iofile(self,name='symbol_sync_out', dir='out', 
+                ionames=['io_symbol_sync_out'] ) 
+        
+        self.IOS.Members['Z'] = IO();              # Pointer for output data
+        _=verilog_iofile(self,
+                name='Z', dir='out', ionames=['io_Z_real', 'io_Z_imag'], datatype='scomplex' )
+        self.IOS.Members['control_write']= IO() 
+        # This is a placeholder, file is created elsewher
+        #_=verilog_iofile(self, name='control_write', dir='in', iotype='file') 
         self.model='py';             # Can be set externally, but is not propagated
         self.par= False              # By default, no parallel processing
         self.queue= []               # By default, no parallel processing
-        self._Z = IO();              # Pointer for output data
+
         if len(arg)>=1:
             parent=arg[0]
             self.copy_propval(parent,self.proplist)
@@ -35,18 +49,14 @@ class ofdm_demodulator(verilog,thesdk):
         self.init()
 
     def init(self):
+        pass
         #This gets updated every time you add an iofile
         ##self.iofile_bundle=Bundle()
         # Define the outputfile
-        _=verilog_iofile(self,name='Z',datatype='complex')
-        _=verilog_iofile(self,name='A',dir='in')
-        _=verilog_iofile(self,name='symbol_sync_in',dir='in')
-        _=verilog_iofile(self,name='symbol_sync_out')
-        self.vlogparameters=dict([ ('g_Rs',self.Rs),])
 
     def main(self):
-        data=self.A.Data
-        syncindex=np.where(self.symbol_sync_in.Data==1)[0]
+        data=self.IOS.Members['A'].Data
+        syncindex=np.where(self.IOS.Members['symbol_sync_in'].Data==1)[0]
         #[TODO]Fasten by memory pre-allocation
         for i in range(syncindex.shape[0]):
             if i==0:
@@ -59,11 +69,11 @@ class ofdm_demodulator(verilog,thesdk):
         ft2=np.r_['1', ft,np.zeros((ft.shape[0],16),dtype=complex)]
         syncout=np.zeros(ft2.shape)
         syncout[:,0]=1
-        self._symbol_sync_out.Data=syncout.reshape(-1,1)
-        self._Z.Data=ft2.reshape(-1,1)
+        self.IOS.Members['symbol_sync_out'].Data=syncout.reshape(-1,1)
+        self.IOS.Members['Z'].Data=ft2.reshape(-1,1)
         if self.par:
-            self.queue.put(self._Z.Data)
-            self.queue.put(self._symbol_sync_out.Data)
+            self.queue.put(self.IOS.Members['Z'].Data)
+            self.queue.put(self.IOS.Members['symbol_sync_out'].Data)
 
     def run(self,*arg):
         if len(arg)>0:
@@ -72,17 +82,9 @@ class ofdm_demodulator(verilog,thesdk):
         if self.model=='py':
             self.main()
         elif self.model=='sv':
-            self.control_write.Data.Members['control_write'].adopt(parent=self)
-
-            # Create testbench and execute the simulation
-            self.define_testbench()
-            self.tb.export(force=True)
-            self.write_infile()
+            self.vlogparameters=dict([ ('g_Rs',self.Rs),])
             self.run_verilog()
-            self.read_outfile()
-            self._Z.Data=self.iofile_bundle.Members['Z'].data
-            self._symbol_sync_out.Data=self.iofile_bundle\
-                    .Members['symbol_sync_out'].data
+
             #This is for parallel processing
             if self.par:
               self.queue.put(out)
@@ -92,110 +94,22 @@ class ofdm_demodulator(verilog,thesdk):
         elif self.model=='vhdl':
             self.print_log(type='F', msg='VHDL model not yet supported')
 
-    def write_infile(self):
-        #Connect IOfiles here
-        self.iofile_bundle.Members['A'].data=self.A.Data.reshape(-1,1)
-        self.iofile_bundle.Members['symbol_sync_in'].data\
-                =self.symbol_sync_in.Data.reshape(-1,1)
-        # This could be a method somewhere
+    def define_io_conditions(self):
+        # Input A is read to verilog simulation after 'initdo' is set to 1 by controller
         for name, val in self.iofile_bundle.Members.items():
-            if val.dir=='in':
-                self.iofile_bundle.Members[name].write()
-
-    def read_outfile(self):
-        # Procedure: 
-        # Define dtypes if not default
-        # Sequence through the outfile
-        for name, val in self.iofile_bundle.Members.items():
-            if val.dir=='out':
-                self.iofile_bundle.Members[name].read()
-
-    # Testbench definition method
-    def define_testbench(self):
-        #Initialize testbench
-        self.tb=vtb(self)
-        # Create TB connectors from the control file
-        for connector in self.control_write\
-                .Data.Members['control_write'].verilog_connectors:
-            self.tb.connectors.Members[connector.name]=connector
-            # Connect them to DUT
-            try: 
-                self.dut.ios.Members[connector.name].connect=connector
-            except:
-                pass
-
-        # Dut is created automaticaly, if verilog file for it exists
-        self.tb.connectors.update(bundle=self.tb.dut_instance.io_signals.Members)
-
-        #Assign verilog simulation parameters to testbench
-        self.tb.parameters=self.vlogparameters
-
-        # Copy iofile simulation parameters to testbench
-        for name, val in self.iofile_bundle.Members.items():
-            self.tb.parameters.Members.update(val.vlogparam)
-
-        # Define the iofiles of the testbench. '
-        # Needed for creating file io routines 
-        self.tb.iofiles=self.iofile_bundle
-
-        #Define testbench verilog file
-        self.tb.file=self.vlogtbsrc
+            if val.dir is 'in':
+                self.iofile_bundle.Members[name].verilog_io_condition='initdone'
+        # Output is read to verilog simulation when all of the utputs are valid, 
+        # and after 'initdone' is set to 1 by controller
+            if val.dir is 'out':
+                self.iofile_bundle.Members[name].verilog_io_condition_append(
+                        cond='&& initdone')
+                if val.name is 'Z':
+                    self.iofile_bundle.Members[name].verilog_io_condition_append(
+                            cond='\n&& ~$isunknown(io_symbol_sync_out)')
 
 
-        ## Start initializations
-        #Init the signals connected to the dut input to zero
-        for name, val in self.tb.dut_instance.ios.Members.items():
-            if val.cls=='input':
-                val.connect.init='\'b0'
 
-        # IO file connector definitions
-        # Define what signals and in which order and format are read form the files
-        # i.e. verilog_connectors of the file
-        # All connectors should be already defined at this phase
-        name='control_write'
-        ionames=[ _.name for _ in self.control_write.Data.Members[name]\
-                .verilog_connectors ]
-        self.iofile_bundle.Members[name].verilog_connectors=\
-                self.tb.connectors.list(names=ionames)
-        
-        #for name in ionames:
-        #    self.tb.connectors.Members[name].type='signed'
-
-        name='A'
-        ionames=[]
-        ionames+=['io_A_real', 'io_A_imag' ]
-        self.iofile_bundle.Members[name].verilog_connectors=\
-                self.tb.connectors.list(names=ionames)
-        self.iofile_bundle.Members[name].verilog_io_condition='initdone'
-
-        name='symbol_sync_in'
-        ionames=[]
-        ionames+=['io_symbol_sync_in']
-        self.iofile_bundle.Members[name].verilog_connectors=\
-                self.tb.connectors.list(names=ionames)
-        self.iofile_bundle.Members[name].verilog_io_condition='initdone'
-
-        name='Z'
-        ionames=[]
-        ionames+=['io_Z_real', 'io_Z_imag' ] 
-        self.iofile_bundle.Members[name].verilog_connectors=\
-                self.tb.connectors.list(names=ionames)
-
-        self.iofile_bundle.Members[name].verilog_io_condition_append(\
-                cond='&& initdone \n&& ~$isunknown(io_symbol_sync_out)' )
-        for name in ionames:
-            self.tb.connectors.Members[name].type='signed'
-
-        name='symbol_sync_out'
-        ionames=[]
-        ionames+=['io_symbol_sync_out'] 
-        self.iofile_bundle.Members[name].verilog_connectors=\
-                self.tb.connectors.list(names=ionames)
-
-        self.iofile_bundle.Members[name].verilog_io_condition_append(\
-                cond='&& initdone')
-        ## This method is in verilog_testbench class
-        self.tb.generate_contents()
 
 if __name__=="__main__":
     import matplotlib.pyplot as plt
@@ -238,17 +152,17 @@ if __name__=="__main__":
         d.init()
         #d.interactive_verilog=True
         d.interactive_verilog=False
-        d.A.Data=data
-        d.symbol_sync_in.Data=symbol_sync
-        d.control_write=controller.control_write
+        d.IOS.Members['A'].Data=data.reshape(-1,1)
+        d.IOS.Members['symbol_sync_in'].Data=symbol_sync.astype('int')
+        d.IOS.Members['control_write']=controller.IOS.Members['control_write']
         d.run()
-        syncindex=np.where(d._symbol_sync_out.Data.astype('int')==1)[0]
+        syncindex=np.where(d.IOS.Members['symbol_sync_out'].Data.astype('int')==1)[0]
         for i in range(syncindex.shape[0]):
             if i==0:
-                received=d._Z.Data[syncindex[i]:syncindex[i]+64].reshape(1,-1)
-            elif syncindex[i]+64<d._Z.Data.shape[0]:
+                received=d.IOS.Members['Z'].Data[syncindex[i]:syncindex[i]+64].reshape(1,-1)
+            elif syncindex[i]+64<d.IOS.Members['Z'].Data.shape[0]:
                 received=np.r_['0', received, \
-                        d._Z.Data[syncindex[i]:syncindex[i]+64].reshape(1,-1)]
+                        d.IOS.Members['Z'].Data[syncindex[i]:syncindex[i]+64].reshape(1,-1)]
         toplot.append(received)
 
     #Plots start here
--- ofdm_demodulator/controller.py
+++ ofdm_demodulator/controller.py
@@ -19,12 +19,11 @@ class controller(verilog,thesdk):
         self.Users = 16;                   # Number of users
         self.symbol_length  = 64;          # OFDM symbol length
         self.time=0
-        self.A = IO();            # Input data, FFT Bins in series 
-        self._Z = IO();           # Output, equalized FFT bins
-        self.control_write = IO()
-        self.control_write.Data = Bundle()
+        self.IOS=Bundle()
+        self.IOS.Members['control_write'] = IO(
+                Data=verilog_iofile(self,name='control_write', dir='in',iotype='event')
+            )
         self.control_read = IO()
-        self.control_read.Data = Bundle()
         self.model='py';             #can be set externally, but is not propagated
         self.par= False              #By default, no parallel processing
         self.queue= []               #By default, no parallel processing
@@ -39,12 +38,6 @@ class controller(verilog,thesdk):
         # Let's read in the file to have IOs defined
         self.dut=verilog_module(file=self.vlogsrcpath 
                 + '/ofdm_demodulator.sv')
-
-        # Scan is the way to pass the controls# 
-        # Format: Time in rows, 
-        # Signals in columns, first column is the timestamp
-        self._scan = IO();           # Pointer for output data
-        self._scan.Data=Bundle()
 
         # Define the signal connectors associeted with this 
         # controller
@@ -78,19 +71,10 @@ class controller(verilog,thesdk):
 
     def init(self):
         self._vlogparameters =dict([('Rs',self.Rs)])
-        # This gets interesting
-        # IO is a file data stucture
-        iofiles_write=[
-                'control_write'
-                ]
-        for name in iofiles_write:
-            self.control_write.Data.Members[name]=verilog_iofile(self,name=name,
-                    dir='in',iotype='ctrl')
-
         self.define_control()
     
     def reset_control_sequence(self):
-        f=self.control_write.Data.Members['control_write']
+        f=self.IOS.Members['control_write'].Data
         self.time=0
         f.data= np.array([])
         f.set_control_data(init=0) # Initialize to zeros at time 0
@@ -115,13 +99,13 @@ class controller(verilog,thesdk):
                 self.connectors.Members[name].init=''
             scansigs_write.append(name) 
 
-        f=self.control_write.Data.Members['control_write']
+        f=self.IOS.Members['control_write'].Data
         f.verilog_connectors=self.connectors.list(names=scansigs_write)
         f.set_control_data(init=0) # Initialize to zeros at time 0
 
     def reset(self):
         #start defining the file
-        f=self.control_write.Data.Members['control_write']
+        f=self.IOS.Members['control_write'].Data
         for name in [ 'reset', ]:
             f.set_control_data(time=self.time,name=name,val=1)
 
@@ -132,7 +116,7 @@ class controller(verilog,thesdk):
             f.set_control_data(time=self.time,name=name,val=0)
 
     def start_datafeed(self):
-        f=self.control_write.Data.Members['control_write']
+        f=self.IOS.Members['control_write'].Data
         for name in [ 'initdone', ]:
             f.set_control_data(time=self.time,name=name,val=1)
         self.step_time()
